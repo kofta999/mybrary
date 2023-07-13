88f5c8905ff3986e901239cc64a104ded4f622ff
kofta999 <mostafaxxx555@gmail.com>
2023-07-13 14:13:00 +0300

    File upload to database

---
+++ routes/books.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/book");
+const Author = require("../models/author");
+const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
+
+
+// Get all books
+router.get("/", async (req, res) => {
+  let query = Book.find();
+  if (req.query.title != null && req.query.title != '') {
+    query = query.regex('title', new RegExp(req.query.title, 'i'))
+  }
+  if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+    query = query.lte('publishDate', req.query.publishedBefore)
+  }
+  if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+    query = query.gte('publishDate', req.query.publishedAfter)
+  }
+
+  try {
+    const books = await query.exec();
+    res.render("books/index", {
+      books: books,
+      searchOptions: req.query
+    });
+  } catch {
+    res.redirect("/");
+  }
+});
+
+// New book form
+router.get("/new", async (req, res) => {
+  renderNewPage(res, new Book());
+});
+
+// New book creation
+router.post("/", async (req, res) => {
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    publishDate: new Date(req.body.publishDate),
+    pageCount: req.body.pageCount,
+    description: req.description
+  });
+
+  saveCover(book, req.body.cover);
+
+  try {
+    const newBook = await book.save();
+    res.redirect("books");
+  } catch (error) {
+    console.log(error)
+    renderNewPage(res, book, true);
+  }
+});
+
+function saveCover(book, coverEncoded) {
+  console.log(book, coverEncoded)
+  if (coverEncoded == null) return
+  const cover = JSON.parse(coverEncoded)
+  if (cover != null && imageMimeTypes.includes(cover.type)) {
+    console.log('here')
+    book.coverImage = new Buffer.from(cover.data, 'base64')
+    book.coverImageType = cover.type
+  }
+}
+
+async function renderNewPage(res, book, hasError = false) {
+  try {
+    const authors = await Author.find({});
+    const params = {
+      authors: authors,
+      book: book
+    };
+    if (hasError) params.errorMessage = "Error creating book";
+    res.render("books/new", params);
+  } catch {
+    res.redirect("books");
+  }
+}
+
+module.exports = router;
